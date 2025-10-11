a5651aef81fd9c4c80d9c80959edf71cb3f2b56f
CaptainEterk <peter.matthias.kim@gmail.com>
2025-10-10 20:50:07 -0700

    Update to v0.7.9-alpha

--- src/omnivoxel/client/game/settings/ConstantGameSettings.java
+++ src/omnivoxel/client/game/settings/ConstantGameSettings.java
@@ -12,7 +12,7 @@ public class ConstantGameSettings {
     public static final int BLOCKS_IN_CHUNK = ((ConstantGameSettings.CHUNK_WIDTH) * (ConstantGameSettings.CHUNK_LENGTH) * (ConstantGameSettings.CHUNK_HEIGHT));
     public static final int BLOCKS_IN_CHUNK_SIDE = ConstantGameSettings.CHUNK_SIZE * ConstantGameSettings.CHUNK_SIZE;
 
-    public static final String DEFAULT_WINDOW_TITLE = "OmniVoxel v0.7.8-alpha";
+    public static final String DEFAULT_WINDOW_TITLE = "OmniVoxel v0.7.9-alpha";
 
     // TODO: Move this to settings
     public static final int MAX_MESH_GENERATOR_THREADS = Runtime.getRuntime().availableProcessors();
@@ -27,7 +27,6 @@ public class ConstantGameSettings {
             render_distance=128
             sensitivity=2f""";
 
-    public static final long AUTO_RECALCULATE_CHUNKS_TIME = 5000;
     public static final String DATA_LOCATION = "";
     public static final String LOG_LOCATION = FILE_LOCATION + ".logs/";
 
@@ -38,7 +37,6 @@ public class ConstantGameSettings {
     // TODO: Move this to settings
     public static final int TARGET_TPS = 60;
     public static final long TICK_LENGTH_NS = 1_000_000_000L / TARGET_TPS;
-    public static final double TICK_LENGTH = TICK_LENGTH_NS / 1_000_000_000.0;
     public static final String GAME_LOCATION = FILE_LOCATION + "games/";
 
     private static String getRootFolder() {
